5b6748a38044d71a489f8219784871ee34cd8a34
Lily <archetypal.0@gmail.com>
2025-03-08 17:16:23 -0500

    Refactored code

--- src/csci201/HiLo.java
+++ src/csci201/HiLo.java
@@ -2,35 +2,6 @@ package csci201;
 import java.util.Random;
 import java.util.Scanner;
 
-/*
-Instructions:
-
-    ~Design and implement an application that plays the Hi-Lo guessing
-    game with numbers.
-
-    ~The program should pick a random number
-    between 1 and 100 (inclusive)
-
-
-    ~and then repeatedly prompt the
-    user to guess the number.
-
-    ~On each guess, report to the user that the or she is correct or that the guess is high or low. 
-    
-	~Continue accepting guesses until the user guesses correctly or chooses to quit.
-
-    ~Use a sentinel value to determine whether the user wants to quit.
-
-
-    ~Count the number of guesses, and report that value when the user
-    guesses correctly.
-
-    ~At the end of each game (by quitting or a correct
-    guess), prompt to determine whether the user wants to play again.
-	    
-	~Continue playing games until the user chooses to stop.
-*/
-
 public class HiLo {
     public static void main(String[]  args) {
 
