0aa2a3cb33af80ae4d5e8841c053e165a94e93f6
Lily <archetypal.0@gmail.com>
2025-03-08 15:15:44 -0500

    Updated HiLo assignment. Commented out LeapYear class for now due to issues when cloning git repo.

--- src/csci201/HiLo.java
+++ src/csci201/HiLo.java
@@ -15,13 +15,13 @@ public class HiLo {
         ~and then repeatedly prompt the
         user to guess the number.
 
-        ~On each guess, report to the user that the or she is correct or that the guess is high or low. Continue ac-
+        On each guess, report to the user that the or she is correct or that the guess is high or low. Continue ac-
         cepting guesses until the user guesses correctly or chooses to quit.
 
-        ~Use a sentinel value to determine whether the user wants to quit.
+        Use a sentinel value to determine whether the user wants to quit.
 
 
-        ~Count the number of guesses, and report that value when the user
+        Count the number of guesses, and report that value when the user
         guesses correctly.
 
         At the end of each game (by quitting or a correct
@@ -30,7 +30,7 @@ public class HiLo {
         */
 
         Random random = new Random();
-        int randomNumb = random.nextInt(100) + 1;
+        int randomNumb;
         int guess;
         int numberOfGuesses = 0;
         int sentinelVal = -1;
@@ -39,12 +39,15 @@ public class HiLo {
         int answer;
         Scanner scanner = new Scanner(System.in);
 
-        //Remove below line after testing
-        System.out.println("Random number: " + randomNumb);
-        System.out.println("To guess the random number I chose, enter a number between 1-100, or enter -1 to quit: ");
-        guess = scanner.nextInt();
+        
 
         do {
+        	randomNumb = random.nextInt(100) + 1;
+        	//Remove below line after testing
+            System.out.println("Random number: " + randomNumb);
+            System.out.println("To guess the random number generated, enter a number between 1-100, "
+            		+ "or enter -1 to quit: ");
+            guess = scanner.nextInt();
             while (guess != sentinelVal) {
                 while ((guess < 1 || guess > 100) && guess != sentinelVal) {
                     System.out.println("Your guess is not within the range of 1-100. Please try again: ");
@@ -57,13 +60,15 @@ public class HiLo {
                     } else {
                         guessIsHigh = false;
                     }
-                    System.out.println("Your guess is " + (guessIsHigh ? "high" : "low") + ". Please guess again: ");
+                    System.out.println("Your guess is " + (guessIsHigh ? "high" : "low") + ". "
+                    		+ "Please guess again: ");
                     guess = scanner.nextInt();
                 }
 //                System.out.println("While loop continued.");
             }
             //System.out.println("While loop ended.");
-            System.out.println("Thanks for playing! Would you like to play again? Enter -1 to fully quit and anything else to keep playing: ");
+            System.out.println("Thanks for playing! Would you like to play again?"
+            		+ "\nEnter -1 to fully quit and anything else to keep playing: ");
             guess = scanner.nextInt();
             if (guess == sentinelVal) {
                 System.out.println("You have decided to quit.");
--- src/csci201/LeapYearTest.java
+++ src/csci201/LeapYearTest.java
@@ -1,14 +1,14 @@
-package csci201;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-class LeapYearTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
-}
+//package csci201;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.Test;
+//
+//class LeapYearTest {
+//
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
+//
+//}
