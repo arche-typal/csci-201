0d6b87949ae449dd6cfe4ee7f240736e207513d8
Lily <archetypal.0@gmail.com>
2025-03-26 01:34:23 -0400

    Finished and tested.

--- src/csci201/SphereStaticMethods.java
+++ src/csci201/SphereStaticMethods.java
@@ -1,10 +1,40 @@
 package csci201;
 
+import java.util.Scanner;
+
 public class SphereStaticMethods {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	final static double PI = Math.PI;
+	static double radius;
+	static double surfaceArea;
+	static double volume;
+	
+	public static double readInput() {
+		System.out.print("Hello, please enter the radius to calculate the aurface area and volume: ");
+		Scanner scanner = new Scanner(System.in);
+		double radius = scanner.nextDouble();
+		scanner.close();
+		return radius;
 	}
-
+	
+	public static double calculateSurfaceArea(double radius) {
+		double surfaceArea = 4 * PI * Math.pow(radius, 2);
+		return surfaceArea;
+	}
+	
+	public static double calculateVolume(double radius) {
+		volume = 4.0/3.0 * PI * Math.pow(radius, 3);
+		return volume;
+	}
+	
+	public static void displayResults(double radius, double surfaceArea, double volume) {
+		System.out.print("The surface area for the radius given  is " + surfaceArea + "."
+				+ "\nThe voume is " + volume + ".");
+	}
+	
+	public static void main(String[] args) {
+		radius = readInput();
+		surfaceArea = calculateSurfaceArea(radius);
+		volume = calculateVolume(radius);
+		displayResults(radius, surfaceArea, volume);
+	}
 }
