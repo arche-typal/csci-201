fac1ed2af8093d002ff6fb181a1f4704cf8b6bf9
Archetypal <archetypal.0@gmail.com>
2025-04-06 06:25:59 -0500

    Asmt 16 initial commit

---
+++ src/csci201/asmt16/Card.java
@@ -0,0 +1,12 @@
+package csci201.asmt16;
+
+/*
+PP 5.13 Design and implement a class called Card that represents a standard playing card. 
+Each card has a suit and a face value. 
+Create a program that deals five random cards. 
+*/
+
+public class Card {
+
+	
+}
---
+++ src/csci201/asmt16/CardDriver.java
@@ -0,0 +1,10 @@
+package csci201.asmt16;
+
+public class CardDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
