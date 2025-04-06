3cca861b2300144fd56b997515a21833978998d8
Archetypal <archetypal.0@gmail.com>
2025-04-06 06:20:04 -0500

    refactored

--- src/csci201/asmt15/Bulb.java
+++ src/csci201/asmt15/Bulb.java
@@ -1,17 +1,4 @@
 package csci201.asmt15;
-/*
-Course event
-Do the programming project: PP 5.9 on page 240 of your textbook.
-
-Submission Instructions: 
-Submit only the source code (*.java) file(s).
-
-
-PP 5.9 Design and implement a class called Bulb that represents a light
-bulb that can be turned on and off. Create a driver class called
-Lights, whose main method instantiates and turns on some Bulb
-objects. 
-*/
 
 public class Bulb {
 	public boolean isOn = false;
