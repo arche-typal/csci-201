5c481defa5f9dd9f5af8cb257a0f98c542edebeb
Archetypal <archetypal.0@gmail.com>
2025-04-07 10:44:55 -0500

    Refactored

--- src/csci201/asmt15/Lights.java
+++ src/csci201/asmt15/Lights.java
@@ -3,6 +3,8 @@ package csci201.asmt15;
 public class Lights {
 
 	public static void main(String[] args) {
+		welcomeMessage();
+		
 		Bulb bulb1 = new Bulb();
 		Bulb bulb2 = new Bulb();
 		Bulb bulb3 = new Bulb();
@@ -13,5 +15,9 @@ public class Lights {
 		System.out.println("The light, bulb1, is " + bulb1.isOnString() 
 		+ ". The light, bulb2, is " + bulb2.isOnString() 
 		+ ". The light, bulb3, is " + bulb3.isOnString() + ".");
+	}
+	
+	public static void welcomeMessage() {
+		System.out.println("Hello, this program create some 'light bulbs'. It will then 'turn on' some of the light bulbs, and then print the status of all lights.\n");
 	}
 }
