ccfc2b6cba279dd1303a2db90c2eb4e3c967f493
Archetypal <archetypal.0@gmail.com>
2025-04-06 19:23:49 -0500

    Updated asmt 16

--- src/csci201/asmt16/Card.java
+++ src/csci201/asmt16/Card.java
@@ -1,5 +1,9 @@
 package csci201.asmt16;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 /*
 PP 5.13 Design and implement a class called Card that represents a standard playing card. 
 Each card has a suit and a face value. 
@@ -26,4 +30,22 @@ public class Card {
 		this.face = face;
 	}
 	
+	public Card generateRandomCard() {
+		Card card = new Card();
+		card.setSuit(generateRandomSuit());
+		card.setFace(generateRandomFace());
+		return card;
+	}
+	
+	public String generateRandomFace() {
+		ArrayList<String> faces = new ArrayList<>(Arrays.asList("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"));
+		Random rand = new Random();
+        return faces.get(rand.nextInt(12));
+	}
+	
+	public String generateRandomSuit() {
+		ArrayList<String> suits = new ArrayList<>(Arrays.asList("Hearts", "Diamonds", "Clubs", "Spades"));
+		Random rand = new Random();
+        return suits.get(rand.nextInt(4));
+	}
 }
--- src/csci201/asmt16/CardDriver.java
+++ src/csci201/asmt16/CardDriver.java
@@ -1,10 +1,27 @@
 package csci201.asmt16;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+Suit	Cards
+Hearts	A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K
+Diamonds	A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K
+Clubs	A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K
+Spades	A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K
+*/
+
 public class CardDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Card card = new Card();
+		
+		Card card1 = card.generateRandomCard();
+		Card card2 = card.generateRandomCard();
+		Card card3 = card.generateRandomCard();
+		Card card4 = card.generateRandomCard();
+		Card card5 = card.generateRandomCard();
+		
+		System.out.println("The random cards dealt are " + card1.face() + " of " + card1.suit() + ", " + card2.face() + " of " + card2.suit() + ", " + card3.face() + " of " + card3.suit() + ", " + card4.face() + " of " + card4.suit() + ", " + card5.face() + " of " + card5.suit() + ".");
 	}
-
 }
