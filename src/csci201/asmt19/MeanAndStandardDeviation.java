0a57a464dd2ad60ca1b94719da506aaf618f9ee4
Archetypal <archetypal.0@gmail.com>
2025-04-21 14:29:21 -0500

    Asmt 19 walk through in class. More work on Asmt 19.

--- src/csci201/asmt19/MeanAndStandardDeviation.java
+++ src/csci201/asmt19/MeanAndStandardDeviation.java
@@ -1,5 +1,7 @@
 package csci201.asmt19;
 
+import java.lang.reflect.Array;
+
 //Design and implement an application that computes and prints the
 //mean and standard deviation of a list of integers xi through x1Z.
 //Assume that there will be no more than 50 input values. Compute
@@ -9,9 +11,12 @@ package csci201.asmt19;
 public class MeanAndStandardDeviation {
 
 	public static void main(String[] args) {
+		//***Enable user to make list
 		int[] list1 = {1,2,3};
 		double result = mean(list1);
 		System.out.println(result);
+		double result2 = standardDeviation(list1, result);
+		System.out.println(result2);
 		
 	}
 	
@@ -19,11 +24,28 @@ public class MeanAndStandardDeviation {
 		double added = 0;
 		for (int i = 0; i <= numList.length-1; i++) {
 			double num = (double) numList[i];
-			added = added + num;
+			added += num;
 		}
 		
 		double mean = added/numList.length;
 		return mean;
 	}
-
+	
+	public static double standardDeviation(int[] numList, double mean) {
+		//subtract mean from numbers
+		//then square each number
+		//then add the numbers
+		//then divide by number of numbers
+		
+		double added = 0;
+		for (int i = 0; i <= numList.length-1; i++) {
+			double num = (double) numList[i];
+			double numSubMean = num - mean;
+			double numSubMeanSquared = Math.pow(numSubMean, 2);
+			added += numSubMeanSquared;
+		}
+		
+		double standardDev = added/numList.length;
+		return standardDev;
+	}
 }
---
+++ src/csci201/asmt19/StandardDeviationInClass.java
@@ -0,0 +1,31 @@
+package csci201.asmt19;
+
+public class StandardDeviationInClass {
+
+	public static void main(String[] args) {
+		int[] list1 = {12, 22, 13, 4, 5, 6, 7, 8, 9, 10};
+		double result = mean(list1);
+		System.out.println("Mean = " + result);
+		double result2 = standardDeviationInClass(list1);
+		System.out.println("Standard Deviation = " + result2);
+	}
+	
+	public static double mean(int[] numList) {
+		double added = 0;
+		for (int i = 0; i <= numList.length-1; i++) {
+			double num = (double) numList[i];
+			added += num;
+		}
+		double mean = added/numList.length;
+		return mean;
+	}
+	
+	public static double standardDeviationInClass(int[] numList) {
+		double mean = mean(numList);
+		double sum = 0;
+		for (int element : numList)
+			sum += Math.pow((element - mean),2);
+		return Math.sqrt(sum / (numList.length - 1));
+	}
+	
+}
